530ed88a2feb5c0b70ec5961f480648246ed5875
dmknpd <soupuabt@gmail.com>
2024-02-14 12:52:42 +0200

    start

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+import "./App.scss";
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return()
+    }
+}
+
+export default App;
