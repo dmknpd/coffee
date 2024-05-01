f45828582ace2403d7499e9a9fce097c7badd96d
dmknpd <soupuabt@gmail.com>
2024-05-01 16:43:22 +0300

    menu links routes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,27 +1,38 @@
+import { Link } from "react-router-dom";
+
 import "./Menu.scss";
 import coffeeLogoWhite from "../img/LogoWhite.svg";
 import coffeeLogoBlack from "../img/LogoBlack.svg";
 
-const Menu = ({ color }) => {
+const Menu = (color) => {
   return (
-    <ul
-      className="menu"
-      style={color === "black" ? { color: "black" } : { color: "white" }}
-    >
+    <ul className="menu">
       <li className="menu__item">
         <img
-          src={color === "black" ? coffeeLogoBlack : coffeeLogoWhite}
+          src={color.color === "black" ? coffeeLogoBlack : coffeeLogoWhite}
           style={
-            color === "black" ? { marginRight: "-5px", bottom: "-2px" } : null
+            color.color === "black"
+              ? { marginRight: "-5px", bottom: "-2px" }
+              : null
           }
           alt="coffee logo"
           className="menu__item-icon"
         />
-        Coffee house
+        <Link to="/" className="menu__item_link" style={color}>
+          Coffee house
+        </Link>
       </li>
 
-      <li className="menu__item">Our coffee</li>
-      <li className="menu__item">For your pleasure</li>
+      <li className="menu__item">
+        <Link to="/about" className="menu__item_link" style={color}>
+          Our coffee
+        </Link>
+      </li>
+      <li className="menu__item">
+        <Link to="/" className="menu__item_link" style={color}>
+          For your pleasure
+        </Link>
+      </li>
     </ul>
   );
 };
